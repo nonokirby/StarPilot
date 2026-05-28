e9dc540f64d843d4c667d47939228e9b65369753
elkoled <elkoled@gmail.com>
2026-05-28 11:36:09 -0700

    adjust fan setpoint
    (cherry picked from commit 269d2189b2383fa5b7c0d5e7cab4fb527f29a636)

--- system/hardware/fan_controller.py
+++ system/hardware/fan_controller.py
@@ -5,6 +5,9 @@ from abc import ABC, abstractmethod
 from openpilot.common.realtime import DT_HW
 from openpilot.common.swaglog import cloudlog
 from openpilot.common.pid import PIDController
+from openpilot.system.hardware import HARDWARE
+
+OFFSET = 0 if HARDWARE.get_device_type() == "mici" else 5
 
 class BaseFanController(ABC):
   @abstractmethod
@@ -27,12 +30,11 @@ class TiciFanController(BaseFanController):
     if ignition != self.last_ignition:
       self.controller.reset()
 
-    error = cur_temp - 75
+    error = cur_temp - (75 + OFFSET)
     fan_pwr_out = int(self.controller.update(
                       error=error,
-                      feedforward=np.interp(cur_temp, [60.0, 100.0], [0, 100])
+                      feedforward=np.interp(cur_temp, [60.0 + OFFSET, 100.0 + OFFSET], [0, 100])
                     ))
 
     self.last_ignition = ignition
     return fan_pwr_out
-
